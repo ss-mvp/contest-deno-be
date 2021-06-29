a60464a35a4759058b045e66cc31a58129ce1e62
Brandon <bran.ramirez.don@gmail.com>
2021-06-29 12:16:49 -0700

    Cleaned up some Deno-only stuff

--- __tests__/testData/enum.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  roles: [{ role: 'user' }, { role: 'admin' }, { role: 'teacher' }],
-  prompts: [
-    { active: true, prompt: 'Here is a test prompt' },
-    { active: false, prompt: 'Here is another test prompt' },
-  ],
-  flags: [{ flag: 'Content' }],
-  sources: [{ source: 'FDSC' }, { source: 'Rumble' }],
-};
