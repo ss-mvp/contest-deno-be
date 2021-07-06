dd016e6637b49034adf48ccab47bd9f1e0e11eb8
Brandon <bran.ramirez.don@gmail.com>
2021-07-06 09:32:05 -0700

    Updated to log config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { env } from './config';
 import loaders from './loaders';
 
 export default async function app__init() {
   const app = express();
+
+  console.log(env);
 
   await loaders({ expressApp: app });
 
