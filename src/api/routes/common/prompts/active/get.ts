714fedf9c41b2640838d35354c3ddbcc5e07ee69
Brandon <bran.ramirez.don@gmail.com>
2021-07-06 09:50:40 -0700

    Removed auth

--- src/api/routes/common/prompts/active/get.ts
+++ src/api/routes/common/prompts/active/get.ts
@@ -3,10 +3,9 @@
 import { Router } from 'express';
 import Container from 'typedi';
 import { Logger } from 'winston';
-import { Prompts, Roles } from '../../../../../interfaces';
+import { Prompts } from '../../../../../interfaces';
 import { PromptModel } from '../../../../../models';
 import { HTTPError } from '../../../../../utils';
-import { authHandler } from '../../../../middlewares';
 
 export default function activePromptRoute__get(route: Router) {
   const logger: Logger = Container.get('logger');
@@ -17,22 +16,18 @@ export default function activePromptRoute__get(route: Router) {
     Prompts.IPrompt, // Response body
     never, // Request body
     never // Query parameters
-  >(
-    '/',
-    authHandler({ roles: [Roles.RoleEnum.teacher, Roles.RoleEnum.admin] }),
-    async (req, res, next) => {
-      try {
-        const [currentPrompt] = await promptModelInstance.get({ active: true });
+  >('/', async (req, res, next) => {
+    try {
+      const [currentPrompt] = await promptModelInstance.get({ active: true });
 
-        // Should throw an error, as this means an issue with our system
-        if (!currentPrompt)
-          throw HTTPError.create(404, 'No prompt currently set!');
+      // Should throw an error, as this means an issue with our system
+      if (!currentPrompt)
+        throw HTTPError.create(404, 'No prompt currently set!');
 
-        res.status(200).json(currentPrompt);
-      } catch (err) {
-        logger.error(err);
-        next(err);
-      }
+      res.status(200).json(currentPrompt);
+    } catch (err) {
+      logger.error(err);
+      next(err);
     }
-  );
+  });
 }
